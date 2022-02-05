902b07afeb13f7a237fbacb71733ebbcb669503d
Eduardo Diaz <eduardo9dza@gmail.com>
2022-02-04 20:25:36 -0600

    se sube trabajo de las sesiones

---
+++ JavaScript_alura/JavaScript-Parte1-Lenguaje-de-la-web-master/js/form.js
@@ -0,0 +1,36 @@
+var botonAdicionar = document.querySelector("#adicionar-paciente");
+
+botonAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adicionar");
+    var nombre = form.nombre.value;
+    var altura = form.altura.value;
+    var peso = form.peso.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nombreTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nombreTd.textContent = nombre;
+    alturaTd.textContent = altura;
+    pesoTd.textContent = peso;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calcularIMC(peso,altura);
+
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabla = document.querySelector("#tabla-pacientes");
+
+    tabla.appendChild(pacienteTr);
+
+});
